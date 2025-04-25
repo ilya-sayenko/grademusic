a017121ad1f7b87f2072d16af5c1b8841c7f1e15
Илья Саенко <insaenko@spasibosb.ru>
2025-04-25 15:10:17 +0300

    fix

--- grademusic-main-service/src/main/java/com/grademusic/main/GradeMusicServiceApplication.java
+++ grademusic-main-service/src/main/java/com/grademusic/main/GradeMusicServiceApplication.java
@@ -1,18 +1,15 @@
 package com.grademusic.main;
 
-import com.grademusic.main.service.cache.AlbumStatisticsCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GradeMusicServiceApplication {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(GradeMusicServiceApplication.class, args);
-		AlbumStatisticsCache albumStatisticsCache = context.getBean(AlbumStatisticsCache.class);
-		var data = albumStatisticsCache.findById("6288a20e-3f11-45b9-a678-3cfe04377b31");
-
-		System.out.println();
+		SpringApplication.run(GradeMusicServiceApplication.class, args);
 	}
 
 }
